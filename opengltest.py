d45986a46b5e745889326e21a55fcee161b6fdc6
Darylgolden <darylgolden@gmail.com>
2021-06-01 08:39:29 +0800

    test

--- opengltest.py
+++ opengltest.py
@@ -3,7 +3,7 @@ from manim.opengl import *
 
 class Test(Scene):
     def construct(self):
-        square = Square(fill_opacity=0, stroke_opacity=1)
+        square = Square(fill_opacity=1, stroke_opacity=1)
 
         self.add(square)
         self.wait()
